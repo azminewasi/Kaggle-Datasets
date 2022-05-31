c84669510786b0f009ec6436ec2fd4321bd2efde
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2022-05-31 15:36:24 +0600

    sg

---
+++ In Process/UCL 2021-22/p-disciplinary.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import pandas as pd
+
+f = open("distributon.txt", "r", encoding='utf-8')
+data=f.readlines()
+
+
+serial=[]
+player=[]
+club=[]
+position=[]
+total_attempts=[]
+on_target=[]
+off_target=[]
+blocked=[]
+match_played=[]
+c11=[]
+c12=[]
+c13=[]
+
+for i in range(0,len(data),13):
+    serial.append(data[i+0].strip())
+    club.append(data[i+2].strip())
+    player.append(data[i+3].strip())
+    position.append(data[i+4].split("-")[1].strip())
+    total_attempts.append(data[i+5].strip())
+    on_target.append(data[i+6].strip())
+    off_target.append(data[i+7].strip())
+    blocked.append(data[i+8].strip())
+    match_played.append(data[i+9].strip())
+    c11.append(data[i+10].strip())    
+    c12.append(data[i+11].strip()) 
+    c13.append(data[i+12].strip()) 
+df_dict={"serial":serial,"player_name":player,"club":club,"position":position,"pass_accuracy":total_attempts,"pass_attempted":on_target,"pass_completed":off_target,"cross_accuracy":blocked,"cross_attempted":match_played,"cross_complted":c11,"freekicks_taken":c12,"match_played":c13}
+
+df=pd.DataFrame.from_dict(df_dict)
+print(df.head())
+
+df.to_csv("distributon.csv",index=False)
